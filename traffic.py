75b5929c841d2812410f2d3e790172ab62ae8811
Kritarth <kritarth2121@gmail.com>
2023-01-01 02:25:59 +0530

    percentage changes

--- traffic.py
+++ traffic.py
@@ -4,6 +4,16 @@ import random
 print("position", pg.position())
 print("size", pg.size())
 
+x, y = pg.size()
+
+
+def percentToPixelX(a):
+    return x*a/100
+
+
+def percentToPixelY(a):
+    return y*a/100
+
 
 def waitedscroll(a, b, scrollGap):
     for s in range(a, random.randrange(a+1, b)):
@@ -17,15 +27,17 @@ def buzziness():
     pg.move(0, -random.randrange(5, 10), random.uniform(0.1, 0.3))
     pg.move(-random.randrange(5, 10), 0, random.uniform(0.1, 0.3))
 
+
 def openQuitTor():
-        pg.hotkey("ctrl", "q")
-        openTor()
+    pg.hotkey("ctrl", "q")
+    openTor()
+
 
 def openTor():
-        pg.hotkey("ctrl", "alt","d")
-        pg.moveTo(1530, 1050)
-        pg.click()
-        time.sleep(6)
+    pg.hotkey("ctrl", "alt", "d")
+    pg.moveTo(1530, 1050)
+    pg.click()
+    time.sleep(6)
 
 
 blogsList = ["https://www.wealthandtech.com/2022/12/start-your-journey-to-becoming-data.html",
@@ -42,57 +54,57 @@ blogsList = ["https://www.wealthandtech.com/2022/12/start-your-journey-to-becomi
              "https://www.wealthandtech.com/2022/12/digital-detox-how-to-kick-your.html",
              "https://www.wealthandtech.com/2022/12/xc.html"]
 
-count=0
+count = 0
 while True:
-    if count==0:
+    if count == 0:
         openTor()
-    elif count%10:
+    elif count % 10:
         openQuitTor()
 
-    count+=1
+    count += 1
 
     pg.hotkey("win", "up")
-    pg.moveTo(1850, 100, 2)
+    pg.moveTo(percentToPixelX(96.35), percentToPixelY(9.25), 2)
     pg.click()
-    
-    pg.moveTo(1610, 620, 2)
+
+    pg.moveTo(percentToPixelX(83.85), percentToPixelY(57.40), 2)
     pg.click()
     time.sleep(2)
 
-    pg.moveTo(500, 100, 2)
+    pg.moveTo(percentToPixelX(26.04), percentToPixelY(9.25), 2)
     pg.click()
     pg.typewrite(blogsList[random.randrange(0, len(blogsList))])
     pg.typewrite(["enter"])
     time.sleep(random.randrange(5, 10))
-    pg.moveTo(random.randrange(200, 1500),
-                      random.randrange(
-                300, 1000), random.uniform(0.25, 0.75))
+    pg.moveTo(random.randrange(percentToPixelX(10), percentToPixelX(90)),
+              random.randrange(
+        percentToPixelY(20), percentToPixelY(90)), random.uniform(0.25, 0.75))
     conditions = [time.sleep(random.randrange(5, 10)),
-            buzziness(),
-            pg.moveTo(random.randrange(200, 1500),
-                      random.randrange(
-                300, 1000), random.uniform(0.25, 0.75)),
-            pg.moveTo(random.randrange(200, 1500),
-                      random.randrange(
-                300, 1000), random.uniform(0.25, 0.75)),
-            waitedscroll(10, 40, -1), buzziness(),
-            time.sleep(random.uniform(1, 10)),
-            waitedscroll(5, 10, 1),
-            buzziness(),
-            time.sleep(random.uniform(1, 10)),
-            buzziness(),
-            time.sleep(random.uniform(1, 10)),
-            waitedscroll(10, 30, -1),
-            waitedscroll(5, 10, 1),
-            buzziness(),
-            ]
+                  buzziness(),
+                  pg.moveTo(random.randrange(percentToPixelX(10), percentToPixelX(90)),
+                            random.randrange(
+                      percentToPixelY(20), percentToPixelY(90)), random.uniform(0.25, 0.75)), pg.moveTo(random.randrange(percentToPixelX(10), percentToPixelX(90)),
+                                                                                                        random.randrange(
+                          percentToPixelY(20), percentToPixelY(90)), random.uniform(0.25, 0.75)),
+                  waitedscroll(10, 40, -1), buzziness(),
+                  time.sleep(random.uniform(1, 10)),
+                  waitedscroll(5, 10, 1),
+                  buzziness(),
+                  time.sleep(random.uniform(1, 10)),
+                  buzziness(),
+                  time.sleep(random.uniform(1, 10)),
+                  waitedscroll(10, 30, -1),
+                  waitedscroll(5, 10, 1),
+                  buzziness(),
+                  ]
     random.shuffle(conditions)
     print(conditions)
     for i in conditions:
         i
 
-    pg.moveTo(1800, 100, random.uniform(0.25, 2))
+    pg.moveTo(percentToPixelX(93.75), percentToPixelY(
+        9.25), random.uniform(0.25, 2))
     pg.click()
 
-    print("done loop",count)
+    print("done loop", count)
     time.sleep(random.randrange(60, 80))
